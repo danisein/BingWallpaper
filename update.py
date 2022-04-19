edd3dd2266cc14c49771b3bf80db25b91aa151e3
gaojianqiang <jason@mediacomm.com.cn>
2022-04-19 15:01:08 +0800

    Can select a monitor to update wallpaper.

--- update.py
+++ update.py
@@ -3,9 +3,9 @@
 import urllib as ulb
 import urllib.request as ulb2
 import json
-import win32gui,win32con,win32api
-from PIL import Image
 import os
+import sys
+import ctypes
 params = {}
 params['format'] = 'js'
 params['idx'] = '0'  #0 for today, 1 for yesterday and so on
@@ -14,21 +14,22 @@ params['uhd'] = '1'
 params['uhdwidth'] = '3840'
 params['uhdheight'] = '2160'
 
+monitorIndex = -1; # 0 for monitor 1, 1 for monitor 2, -1 for all monitor
 def update():
 	#get the json info
 	web_path = "http://www.bing.com/HPImageArchive.aspx"
 	data = ulb.parse.urlencode(params)
 	web_path = web_path + '?' + data
-	# print web_path
+	# print(web_path)
 	response = ulb2.urlopen(web_path)
 	result = response.read()
 	info = json.loads(result)
-	# print info
+	# print(info)
 	#get the image
 	img_path = info['images'][0]['url']
 	if img_path.find('http') < 0:
 		img_path = 'http://www.bing.com' + img_path
-	# print img_path
+	# print(img_path)
 	response = ulb2.urlopen(img_path)
 	result = response.read()
 	if (len(result) > 0):
@@ -41,30 +42,21 @@ def update():
 	else:
 		return ''
 
-
-StoreFolder = "c:\\dayImage"
-
-def setWallpaperFromBMP(imagepath):
-    k = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER,"Control Panel\\Desktop",0,win32con.KEY_SET_VALUE)
-    win32api.RegSetValueEx(k, "WallpaperStyle", 0, win32con.REG_SZ, "2") #2拉伸适应桌面,0桌面居中
-    win32api.RegSetValueEx(k, "TileWallpaper", 0, win32con.REG_SZ, "0")
-    win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER,imagepath, 1+2)
-
 def setWallPaper(imagePath):
-    """
-    Given a path to an image, convert it to bmp and set it as wallpaper
-    """
-    bmpImage = Image.open(imagePath)
-    newPath = StoreFolder + '\\mywallpaper.bmp'
-    try:
-    	os.mkdir(StoreFolder)
-    except:
-    	pass
-    bmpImage.save(newPath, "BMP")
-    setWallpaperFromBMP(newPath)
+	dll = ctypes.windll.LoadLibrary(".\\IDesktopWallpaper.dll")
+	# must be absolute path
+	abspath = os.path.abspath(imagePath)
+	dll.SetWallpaper(-1, abspath)
 
 
 if __name__ == "__main__":
+	if len(sys.argv) > 1:
+		monitorIndex = int(sys.argv[1]);
+	
+	if monitorIndex >= 0:
+		print("Update wallpaper for monitor ", monitorIndex + 1, ".")
+	else:
+		print("Update wallpaper for all monitor.")
 	img_file = update()
 	if img_file != '':
 		setWallPaper(img_file)
